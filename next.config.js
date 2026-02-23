543a2ad6d1b7393a63f7173278445f2f19e42cf3
rubelmollik86 <rubelmollik86@gmail.com>
2026-02-23 14:19:31 +0600

    update:completed the assigned task

--- next.config.js
+++ next.config.js
@@ -4,36 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "i.imgur.com",
-      },
-      {
-        protocol: "https",
-        hostname: "placeimg.com",
-      },
-      {
-        protocol: "https",
-        hostname: "api.lorem.space",
-      },
-      {
-        protocol: "https",
-        hostname: "**.platzi.com",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "i.pravatar.cc",
-      },
-
-      {
-        protocol: "https",
-        hostname: "api.escuelajs.co",
+        hostname: "**",
       },
     ],
   },
